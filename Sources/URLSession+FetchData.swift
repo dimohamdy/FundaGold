098ad61505e75a0bc71c1541d5b947a27989fe1b
dimo.hamdy <dimo.hamdy@gmail.com>
2023-12-20 14:15:31 +0100

    Throw error instead of fatel error

--- Sources/URLSession+FetchData.swift
+++ Sources/URLSession+FetchData.swift
@@ -12,12 +12,12 @@ import FoundationNetworking
 extension URLSession {
     func fetchData(for request: URLRequest) async throws -> (Data, URLResponse) {
         return await withCheckedContinuation { continuation in
-            self.dataTask(with: request) { data, response, _ in
+            self.dataTask(with: request) { data, response, error in
                 if let data = data, let response = response {
                     continuation.resume(returning: (data, response))
                 } else {
                     // Handle unexpected case
-                    fatalError("Unexpected result from dataTask")
+                    throw error
                 }
             }.resume()
         }
